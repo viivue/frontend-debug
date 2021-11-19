6b0a5652d27cf2d6962741fbac4bfa8aeb2e363b
phucbm <hiphucbui@gmail.com>
2021-11-19 22:08:13 +0700

    :wrench: indicate new changed value

--- fe-debug.js
+++ fe-debug.js
@@ -14,14 +14,16 @@
             this.lastSpeedCount = 0;
             this.lastSpeedTotal = 0;
 
+            this.memory = {};
+
             this.stats = [
                 {
                     slug: 'scroll',
                     label: 'Scroll: [value]',
                     value: () => {
                         const direction = this.lastScrollPosition > this.scroll().top ? '⏫' : '⏬';
-                        const progress = this.scroll().top / (document.body.clientHeight - this.viewport().h);
-                        return `${this.round(this.scroll().top)} ${direction} ${this.round(progress)}`;
+                        const progress = this.round(this.scroll().top / (document.body.clientHeight - this.viewport().h), 3);
+                        return `${this.indicate(this.round(this.scroll().top), 'scrollAmount')} ${direction} ${this.indicate(progress, 'progress')}`;
                     }
                 },
                 {
@@ -35,15 +37,19 @@
                         this.lastSpeedCount++;
                         this.lastSpeedTotal += this.lastSpeed;
 
-                        return this.round(this.lastSpeed);
+                        return this.indicate(this.round(this.lastSpeed), 'lastSpeed');
                     }
                 },
                 {
                     slug: 'average-speed',
                     label: 'Avg. speed: [value]',
                     value: () => {
-                        this.averageSpeed = this.lastSpeedTotal / this.lastSpeedCount;
-                        return this.round(this.averageSpeed);
+                        // only update if changes
+                        if(this.lastScrollPosition !== this.scroll().top){
+                            this.averageSpeed = this.lastSpeedTotal / this.lastSpeedCount;
+                        }
+
+                        return this.indicate(this.round(this.averageSpeed), 'averageSpeed');
                     }
                 },
                 {
@@ -51,20 +57,20 @@
                     label: 'Max speed: [value]',
                     value: () => {
                         this.maxSpeed = Math.max(this.maxSpeed, this.lastSpeed);
-                        return this.round(this.maxSpeed);
+                        return this.indicate(this.round(this.maxSpeed), 'maxSpeed');
                     }
                 },
                 {
                     separator: true,
                     slug: 'viewport',
                     label: 'Viewport: [value]',
-                    value: () => `${this.viewport().w}/${this.viewport().h}`
+                    value: () => `${this.indicate(this.viewport().w, 'viewportWidth')}/${this.indicate(this.viewport().h, 'viewportHeight')}`
                 },
 
                 {
                     slug: 'document',
                     label: 'Document: [value]',
-                    value: () => `${document.body.clientWidth}/${document.body.clientHeight}`
+                    value: () => `${this.indicate(document.body.clientWidth, 'clientWidth')}/${this.indicate(document.body.clientHeight, 'clientHeight')}`
                 }
             ];
 
@@ -79,15 +85,39 @@
             window.requestAnimationFrame(onUpdate);
         }
 
-        updateStats(){
-            // exit if scroll position not moving
-            if(this.lastScrollPosition === this.scroll().top) return;
+        indicate(value, key){
+            const newValue = value => `<span style="color:#96cdff">${value}</span>`;
 
+            if(typeof this.memory[key] === 'undefined'){
+                // create new
+                this.memory[key] = {};
+            }else{
+                // found
+                if(this.memory[key].value === value){
+                    // same value
+                    return this.memory[key].isNew ? newValue(value) : value;
+                }
+            }
+
+
+            // update value
+            this.memory[key].value = value;
+            this.memory[key].isNew = true;
+
+            // setTimeout
+            clearTimeout(this.memory[key].timeout);
+            this.memory[key].timeout = setTimeout(() => {
+                this.memory[key].isNew = false;
+            }, this.indicateTime);
+
+            return newValue(value);
+        }
+
+        updateStats(){
             // loop through all stats
             for(const item of this.stats){
                 this.debugContainer.find(`[data-fe-debug="${item.slug}"]`).html(item.label.replace('[value]', item.value()));
             }
-
 
             this.lastScrollPosition = this.scroll().top;
         }
@@ -138,13 +168,13 @@
          */
         validate(){
             // stop debug
-            const isStopDebug = window.location.search.indexOf('nodebug') !== -1;
+            const isStopDebug = this.getUrlParam('nodebug') !== null;
             if(isStopDebug){
                 sessionStorage.removeItem("FrontEndDebug");
                 return false;
             }
 
-            const isDebug = window.location.search.indexOf('debug') !== -1;
+            const isDebug = this.getUrlParam('debug') !== -1;
             const notInit = !this.debugContainer.length;
             const isPassed = isDebug && notInit;
 
@@ -196,6 +226,16 @@
 
         round(number = 0, fractionDigits = 2){
             return parseFloat(number.toFixed(fractionDigits));
+        }
+
+        /**
+         * Get parameter from URL
+         * @param param
+         * @param url
+         * @returns {*}
+         */
+        getUrlParam(param, url = window.location.href){
+            return new URL(url).searchParams.get(param);
         }
     }
 
