3a35b0e56e4b4e36b7b3bee6e9e141f7ccb2fad4
vupham <vuquangpham2909@gmail.com>
2022-10-14 18:51:47 +0700

    :zap: upTime > enhance filter domain function

--- src/upTime.js
+++ src/upTime.js
@@ -16,6 +16,10 @@ const isLastAccessPage = () => {
     if(!urlObjStorage) return false;
 
     urlObj = urlObjStorage;
+    if(!urlObj.urlList.find(url => url === getUrl()))
+        urlObj.urlList.push(getUrl());
+    urlObj.url = getDomain(urlObj.url, urlObj.urlList);
+
     return true;
 };
 
@@ -28,13 +32,9 @@ const checkLastAccessPage = () => {
         currentTime = Date.now() - urlObj.totalTime;
         return;
     }
-    const urlInstance = new URL(location.href);
     urlObj = {
-        host: urlInstance.host,
-        hostname: urlInstance.hostname,
-        href: urlInstance.href,
-        origin: urlInstance.origin,
-        pathname: urlInstance.pathname
+        url: getUrl(),
+        urlList: [getUrl()],
     };
 };
 
@@ -73,4 +73,107 @@ export const getDiffTime = (currentDate, previousDate = currentTime) => {
  */
 export const getRealTime = (currentDate, previousDate = realTime) => {
     return getDiffTime(currentDate, previousDate);
+};
+
+/**
+ * Get URL string
+ * @returns string
+ */
+const getUrl = () => {
+    const url = new URL(location.href);
+    return url.origin + url.pathname + '/';
+};
+
+
+/**
+ * Add slash to the last character
+ * @param arr
+ * @returns array
+ */
+const addSlashToURL = (arr) => {
+    return arr.map(str => {
+        if(str.slice(-1) === '/') return str;
+        return str + '/';
+    });
+};
+
+
+/**
+ * Get the same string from 2 strings
+ * @param firstString
+ * @param secondString
+ * @returns array
+ */
+const getSameString = (firstString, secondString) => {
+    const fStrArray = firstString.split('/');
+    const sStrArray = secondString.split('/');
+
+    let newStr = '';
+
+    for(let i = 0; i < Math.min(fStrArray.length, sStrArray.length); i++){
+        const word = fStrArray[i];
+        if(word !== sStrArray[i]) return newStr;
+        newStr += word + '/';
+    }
+    return newStr.slice(0, -1);
+};
+
+
+/**
+ * Get different from domain list
+ * @param urlArray
+ * @returns array
+ */
+const getDiffDomainFromURL = (urlArray) => {
+    if(!urlArray) return;
+    if(urlArray.length === 1) return urlArray;
+
+    const newUrlArray = addSlashToURL(urlArray);
+    const results = [];
+
+    let i = 0, j = 0;
+    while(i < newUrlArray.length - 1){
+        const str1 = newUrlArray[i];
+        j = i + 1;
+        while(j < newUrlArray.length){
+            const str2 = newUrlArray[j++];
+            const newStr = getSameString(str1, str2);
+
+            // not contains the result of compare string  and not a valid URL
+            if(!newStr || newUrlArray.find(str => str === newStr) === undefined)
+                continue;
+
+            const resultIdx = results.find(str => str === newStr);
+            if(!resultIdx){
+                const idx = results.findIndex(str => str.includes(newStr) || newStr.includes(str));
+
+                // not exist yet
+                if(idx === -1){
+                    results.push(newStr);
+                    continue;
+                }
+
+                // already exist
+                if(results[idx].includes(newStr)){
+                    results[idx] = newStr;
+                }
+            }
+        }
+        i++;
+    }
+    return results;
+};
+
+
+/**
+ * Get home-domain from domain list
+ * @param url
+ * @param referenceUrls
+ * @returns string
+ */
+const getDomain = (url, referenceUrls = []) => {
+    if(!url) return '';
+    if(!referenceUrls) return url.split('/')[0] + '/';
+
+    return getDiffDomainFromURL([url, ...referenceUrls])[0];
 };
