b2233675f1dce0df3752bb5f03567d739e339400
phucbm <hiphucbui@gmail.com>
2023-08-04 14:38:25 +0700

    chore: test update body class

--- dev/script.js
+++ dev/script.js
@@ -5,6 +5,7 @@ import 'honcau';
 
 // source script
 import '@/_index';
+import {addClass, removeClass} from "@/utils";
 
 // import package info
 const packageInfo = require('../package.json');
@@ -17,3 +18,13 @@ const title = `${packageInfo.prettyName} v${packageInfo.version}`;
 document.title = `${title} - ${packageInfo.description}`;
 document.querySelector('[data-title]').innerHTML = title;
 document.querySelector('[data-description]').innerHTML = packageInfo.description;
+
+
+// test update body class
+let testIndex = 0, bodyEl = document.querySelector('body');
+setInterval(() => {
+    removeClass(bodyEl, `test-class-${testIndex}`);
+
+    testIndex += 1;
+    addClass(bodyEl, `test-class-${testIndex}`);
+}, 3000);
